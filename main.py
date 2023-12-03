3a28e09cd4a0524b6ce439b3be3f06de10da6e26
Shreenidhi Bhat <115147925+205062@users.noreply.github.com>
2023-12-03 17:22:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 def count_batteries_by_health(present_capacities):
   # declaring dictionary to store batteries count
-  d = {
+  batteries = {
     "healthy": 0,
     "exchange": 0,
     "failed": 0
@@ -9,27 +9,27 @@ def count_batteries_by_health(present_capacities):
   
   for i in present_capacities:
     # calculating SoH
-    j = 100 * i/120
+    soh = 100 * i/120
 
     # clasification
-    if 80<=j<=100:
+    if 80<=soh<=100:
       d["healthy"]+=1
-    elif 62<=j<80:
+    elif 62<=soh<80:
       d["exchange"]+=1
     else:
       d["failed"]+=1
       
-  return d
+  return batteries
 
 
 
 def test_bucketing_by_health():
   print("Counting batteries by SoH...\n")
-  present_capacities = [113, 116, 80, 95, 92, 70,70]
+  present_capacities = [113, 116, 80, 95, 92, 70]
   counts = count_batteries_by_health(present_capacities)
   assert(counts["healthy"] == 2)
   assert(counts["exchange"] == 3)
-  assert(counts["failed"] == 2)
+  assert(counts["failed"] == 1)
   print("Done counting :)")
 
 
